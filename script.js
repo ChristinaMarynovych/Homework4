b039a42c48e3e4b76aac02329776b2d8159b659a
Khristina <christina.marynovych@gmail.com>
2020-11-25 18:16:21 +0200

    add homework 4

---
+++ script.js
@@ -0,0 +1,42 @@
+const students = ['Олександр', 'Ігор', 'Олена', 'Іра', 'Олексій', 'Світлана'];
+const themes = ['Диференціальне рівняння', 'Теорія автоматів', 'Алгоритми і структура даних'];
+const marks = [4, 5, 5, 3, 4, 5];
+
+const group1 = students.slice(4);
+const group2 = students.slice(1, 3);
+const group3 = [students[0], students[3]];
+   
+//    console.log(group1);
+//    console.log(group2);
+//    console.log(group3);
+
+const groups = [group1, group2, group3];
+console.log(groups);
+
+const groupsWithThemes = function(groups, themes){
+	const projects = [];
+	for (let i = 0; i < groups.length && i < themes.length; i++){
+		projects.push([groups[i].join(' і '),themes[i]]);
+	}
+	return projects;
+}
+console.log(groupsWithThemes(groups, themes))
+const groupsWork = groupsWithThemes(groups, themes)
+
+const studentsWithMarks = function(students, marks){
+  const studentsPerformance = [];
+  for (let i = 0; i < students.length && i < marks.length; i++){
+    studentsPerformance.push([students[i], marks[i]]);
+  }
+  return studentsPerformance;
+}
+console.log(studentsWithMarks(students, marks));
+
+const groupsWithMarks = function(groupsWork, marks){
+  const groupsPerformance = [];
+  for (let i = 0; i < groupsWork.length; i++){
+    groupsPerformance.push([groupsWork[i].join(' і '), marks[Math.floor(Math.random() * marks.length)]]);
+  }
+  return groupsPerformance
+}
+console.log(groupsWithMarks(groupsWork, marks));
